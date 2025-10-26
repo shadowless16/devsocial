a5a4b09b9a505f4bba457372ca04dba442dba782
Ak David <akdavid4real@gmail.com>
2025-10-26 09:53:58 +0100

    Removed unsed packges

--- components/modals/avatar-modal.tsx
+++ components/modals/avatar-modal.tsx
@@ -2,9 +2,7 @@
 
 import React from 'react'
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
-import { Avatar3D } from '@/components/ui/avatar-3d'
-import { Avatar } from '@/components/ui/avatar'
-import { AvatarImage } from '@/components/ui/avatar'
+import { Avatar, AvatarImage } from '@/components/ui/avatar'
 import { getAvatarUrl } from '@/lib/avatar-utils'
 
 interface AvatarModalProps {
@@ -15,9 +13,6 @@ interface AvatarModalProps {
 }
 
 export function AvatarModal({ isOpen, onClose, avatarUrl, username }: AvatarModalProps) {
-  const base = avatarUrl ? String(avatarUrl).split('?')[0] : ''
-  const is3D = base.endsWith('.glb')
-
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className="max-w-md w-full">
@@ -26,15 +21,9 @@ export function AvatarModal({ isOpen, onClose, avatarUrl, username }: AvatarModa
         </DialogHeader>
         
         <div className="flex justify-center p-4">
-          {is3D ? (
-            <div className="w-80 h-80">
-              <Avatar3D modelUrl={avatarUrl} />
-            </div>
-          ) : (
-            <Avatar className="w-80 h-80">
-              <AvatarImage src={getAvatarUrl(avatarUrl)} alt={`${username}'s avatar`} />
-            </Avatar>
-          )}
+          <Avatar className="w-80 h-80">
+            <AvatarImage src={getAvatarUrl(avatarUrl)} alt={`${username}'s avatar`} />
+          </Avatar>
         </div>
       </DialogContent>
     </Dialog>
